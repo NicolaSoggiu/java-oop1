9af68748aef0ad09870ab9c8060ba0fe7039f000
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-20 14:56:24 +0200

    print first product

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,7 +1,26 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci il nome del prodotto: ");
+        String name = scan.nextLine();
+        System.out.println("Inserisci la descrizione del prodotto: ");
+        String description = scan.nextLine();
+        System.out.println("Inserisci il prezzo del prodotto: ");
+        int price = scan.nextInt();
+        System.out.println("Inserisci IVA del prodotto: ");
+        int IVA = scan.nextInt();
 
+        Prodotto product = new Prodotto(name, description, price, IVA);
+        String finalProduct =   product.getName() + product.getDescrizione() +
+                                product.getPrezzo() + product.getIVA();
+
+        System.out.println( product.getCodice() + finalProduct);
+
+
+        scan.close();
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,43 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
+    Random rand = new Random();
+
+    // attributi
+    private int codice;
+    private String name;
+    private String descrizione;
+    private int prezzo;
+    private int IVA;
+
+    // costruttori
+    public Prodotto(String name, String descrizione, int prezzo, int IVA) {
+        codice = rand.nextInt(1,500);
+        this.name = name;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.IVA = IVA;
+    }
+
+    // metodi
+    public int getCodice() {
+        return codice;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
+
+    public int getIVA() {
+        return IVA;
+    }
 }
